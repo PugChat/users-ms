47b69cdc2a4b654c1d79ca097328b260f162b560
Carlos Augusto Tovar Bonilla <catovar360@gmail.com>
2019-10-17 22:32:21 -0500

    feat: using MySQL

--- app/serializers/user_serializer.rb
+++
@@ -1,4 +0,0 @@
-class UserSerializer < ActiveModel::Serializer
-  include Rails.application.routes.url_helpers
-  attributes :id, :mail, :username, :verification, :active
-end
--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  #post 'user_token' => 'user_token#create'
-  get "users/index"
-  get "users/show"
-  post "users/create"
-  put "users/updated"
-  resources :users
-end
