68324e920970325b73787b0d54b16aa649e5c616
Carlos Augusto Tovar Bonilla <catovar360@gmail.com>
2019-10-27 21:57:48 -0500

    feat: find User by username

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,11 @@ class UsersController < ApplicationController
         render json: user, status: 200
     end
 
+    def findByUsername
+        user = User.find_by(:username => params[:username])
+        render json: user, status: 200
+    end
+
     def create
         user = User.new(user_params)
         if user.save
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   #post 'user_token' => 'user_token#create'
   get "users/index"
+  get "users/findByUsername"
   get "users/show"
   post "users/create"
   put "users/updated"
